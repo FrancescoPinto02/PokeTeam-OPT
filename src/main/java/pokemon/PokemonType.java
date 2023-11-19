f90d4f09ba42a1a57f1af43d199592c2b619fa99
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-19 22:24:00 +0100

    Upload progetto + enum per tipi pokemon

---
+++ src/main/java/pokemon/PokemonType.java
@@ -0,0 +1,6 @@
+package pokemon;
+
+//pokemon types updated at Pokemon Scarlet/Violet
+public enum PokemonType {
+    NORMAL, FIGHTING, FLYING, POISON, GROUND, ROCK, BUG, GHOST, STEEL, FIRE, WATER, GRASS, ELECTRIC, PSYCHIC, ICE, DRAGON, DARK, FAIRY
+}
