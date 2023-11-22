4c819fb54c877dcb0d2982237ba27b0269461069
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-23 00:17:53 +0100

    Aggiunta classe Pokedex + aggiunta PokemonType.UNDEFINED nella enum

---
+++ src/main/java/pokemon/Pokedex.java
@@ -0,0 +1,10 @@
+package pokemon;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class PokemonPool {
+
+    private TypePool typePool;
+    private HashMap<Integer, Pokemon> pokemons;
+}
