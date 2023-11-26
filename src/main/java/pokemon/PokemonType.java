4a46ca5bfda6b69b40afb9bc25eacc471d2bd9e1
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-27 00:30:46 +0100

    Modifiche alle classi Pokemon e Pokedex per usare le classi del nuovo package pokemon.type

--- src/main/java/pokemon/PokemonType.java
+++
@@ -1,6 +0,0 @@
-package pokemon;
-
-//pokemon types updated at Pokemon Scarlet/Violet
-public enum PokemonType {
-    NORMAL, FIGHTING, FLYING, POISON, GROUND, ROCK, BUG, GHOST, STEEL, FIRE, WATER, GRASS, ELECTRIC, PSYCHIC, ICE, DRAGON, DARK, FAIRY, UNDEFINED
-}
