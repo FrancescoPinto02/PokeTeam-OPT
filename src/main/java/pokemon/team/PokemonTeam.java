30fb8d7417b0d823c2c0ce22413b7084b61836c7
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-27 23:21:27 +0100

    Aggiunta implementazione SixPokemonTeam

--- src/main/java/pokemon/team/PokemonTeam.java
+++ src/main/java/pokemon/team/PokemonTeam.java
@@ -4,6 +4,7 @@ import pokemon.Pokemon;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public abstract class PokemonTeam {
     private List<Pokemon> members;
@@ -12,7 +13,36 @@ public abstract class PokemonTeam {
         this.members = new ArrayList<>();
     }
 
+    public List<Pokemon> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Pokemon> members) {
+        this.members = members;
+    }
+
     public abstract boolean addMember(Pokemon pokemon);
     public abstract boolean removeMember(Pokemon pokemon);
     public abstract boolean changeMember(Pokemon pokemonToChange, Pokemon newPokemon);
+    public abstract boolean isFull();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PokemonTeam that = (PokemonTeam) o;
+        return Objects.equals(members, that.members);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(members);
+    }
+
+    @Override
+    public String toString() {
+        return "PokemonTeam{" +
+                "members=" + members +
+                '}';
+    }
 }
---
+++ src/main/java/pokemon/team/SixPokemonTeam.java
@@ -0,0 +1,42 @@
+package pokemon.team;
+
+import pokemon.Pokemon;
+
+import java.util.List;
+
+public class SixPokemonTeam extends PokemonTeam{
+
+    private static final int MAX_TEAM_MEMBERS = 6;
+
+    public SixPokemonTeam(List<Pokemon> members){
+        super();
+        for(int i=0; i < members.size() && !isFull(); i++){
+            addMember(members.get(i));
+        }
+    }
+
+    @Override
+    public boolean addMember(Pokemon pokemon) {
+        if(isFull()){
+            return false;
+        }
+        else{
+            return this.getMembers().add(pokemon);
+        }
+    }
+
+    @Override
+    public boolean removeMember(Pokemon pokemon) {
+        return this.getMembers().remove(pokemon);
+    }
+
+    @Override
+    public boolean changeMember(Pokemon pokemonToChange, Pokemon newPokemon) {
+        return removeMember(pokemonToChange) && addMember(newPokemon);
+    }
+
+    @Override
+    public boolean isFull() {
+        return this.getMembers().size() >= MAX_TEAM_MEMBERS;
+    }
+}
