94b3ef54cb124798ff14db9ec9eb2401e0827b9e
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-27 22:27:44 +0100

    Documentazione PokemonTypeName

--- src/main/java/pokemon/type/PokemonTypeName.java
+++ src/main/java/pokemon/type/PokemonTypeName.java
@@ -1,5 +1,9 @@
 package pokemon.type;
 
+/**
+ * Types` name from Pokemon Games updated to Pokemon Scarlet/Violet
+ * PokemonTypeName.UNDEFINED should be used where the type is unknown or null
+ */
 public enum PokemonTypeName {
     NORMAL, FIGHTING, FLYING, POISON, GROUND, ROCK, BUG, GHOST, STEEL, FIRE, WATER, GRASS, ELECTRIC, PSYCHIC, ICE, DRAGON, DARK, FAIRY, UNDEFINED
 }
