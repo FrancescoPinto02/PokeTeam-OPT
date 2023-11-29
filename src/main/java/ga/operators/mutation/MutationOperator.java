2a0256545d8d8dbc31a83fd2ad71f0cb89d0efc7
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-29 22:38:53 +0100

    Aggiunta classe astratta MutationOperator

---
+++ src/main/java/ga/operators/mutation/MutationOperator.java
@@ -0,0 +1,7 @@
+package ga.operators.mutation;
+
+import ga.individuals.Individual;
+import ga.operators.GeneticOperator;
+
+public abstract class MutationOperator<T extends Individual> extends GeneticOperator<T> {
+}
