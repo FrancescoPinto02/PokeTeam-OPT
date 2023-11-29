e182712b2e2f21a9b5d2b0d2a517e1ed56e21b18
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-29 22:32:23 +0100

    Aggiunta classe astratta SelectionOperator

---
+++ src/main/java/ga/operators/selection/SelectionOperator.java
@@ -0,0 +1,8 @@
+package ga.operators.selection;
+
+import ga.individuals.Individual;
+import ga.operators.GeneticOperator;
+
+public abstract class SelectionOperator<T extends Individual> extends GeneticOperator<T> {
+
+}
