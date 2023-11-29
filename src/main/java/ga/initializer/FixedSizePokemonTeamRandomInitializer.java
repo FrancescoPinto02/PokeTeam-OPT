d32c67da58da7fc79d86eefd4afc6751278a6188
FrancescoPinto02 <francesco.pintop@gmail.com>
2023-11-29 23:00:28 +0100

    Aggiunta implementazione Initializer: FixedSizePokemonTeamRandomInitializer

---
+++ src/main/java/ga/initializer/FixedSizePokemonTeamRandomInitializer.java
@@ -0,0 +1,26 @@
+package ga.initializer;
+
+import ga.individuals.PokemonTeam;
+import ga.population.FixedSizePopulation;
+import ga.population.Population;
+import pokemon.team.PokemonTeamGenerator;
+
+public class FixedSizePokemonTeamRandomInitializer extends Initializer<PokemonTeam>{
+
+    private final int numberOfIndividuals;
+
+    public FixedSizePokemonTeamRandomInitializer(int numberOfIndividuals) {
+        this.numberOfIndividuals = Math.max(numberOfIndividuals, 1);
+    }
+
+    @Override
+    public Population<PokemonTeam> initialize() {
+        FixedSizePopulation<PokemonTeam> population = new FixedSizePopulation<>(0, numberOfIndividuals);
+        for(int i = 0; i < numberOfIndividuals; i++){
+            PokemonTeam individual = PokemonTeamGenerator.generatePokemonTeam(PokemonTeam.MAX_TEAM_MEMBERS);
+            population.add(individual);
+        }
+
+        return population;
+    }
+}
