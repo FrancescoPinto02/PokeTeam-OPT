b3047b7819dac7902a8f908a0d7a6bf89958408a
FrancescoPinto02 <francesco.pintop@gmail.com>
2024-01-18 13:02:47 +0100

    documentazione package pokemon.team + modifiche

--- src/main/java/pokemon/team/PokemonTeamGenerator.java
+++ src/main/java/pokemon/team/PokemonTeamGenerator.java
@@ -7,9 +7,19 @@ import pokemon.core.PokemonGenerator;
 import java.util.ArrayList;
 import java.util.List;
 
-public class PokemonTeamGenerator {
+/**
+ * Classe che gestisce la generazione automatica di squadre Pokemon.
+ */
+public final class PokemonTeamGenerator {
 
+    private PokemonTeamGenerator(){}
 
+    /**
+     * Genera una squadra di Pokémon con la dimensione specificata.
+     *
+     * @param size Dimensione della squadra che si desidera generare.
+     * @return Team Pokemon generato
+     */
     public static PokemonTeam generatePokemonTeam(int size){
         List<Pokemon> team = new ArrayList<>();
         for(int i=0; i<size; i++){
